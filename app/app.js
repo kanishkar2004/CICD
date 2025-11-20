11cb1211f65ad6bc8a1b8ca92ebcb88cce3ef4b8
kanishkar2004 <kanishkarsenthilkumar@gmail.com>
2025-11-21 02:36:36 +0530

    changed -CICD app

--- app/app.js
+++ app/app.js
@@ -3,6 +3,6 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello from CI/CD Pipeline on AWS ECS!\n');
+  res.end('Hello from CI/CD Pipeline on AWS ECS! by-Kanishkar S\n');
 });
 server.listen(port, () => console.log(`Listening on ${port}`));
