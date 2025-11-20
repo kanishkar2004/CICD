b464ede137871dfd59c0a0f19a539055859c1849
kanishkar2004 <kanishkarsenthilkumar@gmail.com>
2025-11-21 03:27:36 +0530

    new app.js1

--- app/app.js
+++ app/app.js
@@ -1,17 +1,8 @@
 // simple Node server
 const http = require('http');
 const port = process.env.PORT || 3000;
-
 const server = http.createServer((req, res) => {
-  if (req.url === '/health') {
-    // Health check endpoint
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('OK'); // ALB expects a 200 response
-  } else {
-    // Default response
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Hello from CI/CD Pipeline on AWS ECS! by-Kanishkar S\n');
-  }
+  res.writeHead(200, {'Content-Type':'text/plain'});
+  res.end('Hello from CI/CD Pipeline on AWS ECS! by-Kanishkar S\n');
 });
-
 server.listen(port, () => console.log(`Listening on ${port}`));
